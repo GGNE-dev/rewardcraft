3ca24da4d9142cdd1f867f327d4216bf734f84df
GGNE <z_zzlzl@naver.com>
2026-04-28 17:53:26 +0900

    Initial Commit

---
+++ src/main/java/org/ggne/rc/RewardcraftApplication.java
@@ -0,0 +1,13 @@
+package org.ggne.rc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardcraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RewardcraftApplication.class, args);
+    }
+
+}
